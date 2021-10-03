52387fa083b1ccd119ab51020a24b7c2c21943ea
prochell <prochell@student.21-school.ru>
2021-10-03 14:25:54 +0300

    add few files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class HelloWorld {
     public static void main(String [] args) {
-        System.out.println("Hello motherfuckers!");
+        int myInt1 = 10;
+        short myInt2;
+        long myInt3;
+        double myInt4;
+        float myInt5;
+
+        char c = 'c';
+        System.out.println(myInt1);
     }
+    /*
+    byte (целые числа, 1 байт)  -128 до 127
+    short (целые числа, 2 байта) -32768 до 32767
+    int (целые числа, 4 байта) -2147483648 до 2147483647
+    long (целые числа, 8 байтов) -9223372036854775808 до 9223372036854775807
+    float (вещественные числа, 4 байта)
+    double (вещественные числа, 8 байтов)
+    char (символ Unicode, 2 байта)
+    boolean (значение истина/ложь, 1 байт)
+*/
 }
