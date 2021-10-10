9cf96a6ee160d479ede611a4684535eca7104d48
prochell <prochell@student.21-school.ru>
2021-10-10 13:41:31 +0300

    fill funcs

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,29 +1,44 @@
 public class MyProjects {
     public static void main(String [] args) {
         Person person1 = new Person();
-        String s1 = "Mike";
-        person1.setName(s1);
-        person1.setAge(20);
+        person1.setName("Kira");
+        person1.setAge(15);
         person1.sayHello();
         person1.speak();
+        System.out.println(person1.getAge());
+        System.out.println(person1.getName());
     }
     /*
-    * Обращаться к параметра объектов можно напрямую через
-    * person1.age = 10;
-    * Но данный подход нарушает принцип инкапусуляции
-    * Правильнее обращаться через метод для избежания ввода
-    * не корректных данных
+    * Пользователю необходимо давать только внешний интерфейс
+    * устройства программы, который не затрагивает значения переменных,
+    * к тому же можно ограничивать ввод
     */
     static class Person{
-        String  name;
-        int     age;
+        private String  name; //private - переменная видна только в пределах класса
+        private int     age;
 
-        void    setName(String userName){
-            name = userName;
+        // Setter - устанавливает значения
+        public void    setName(String userName){
+            if (userName.isEmpty())
+                System.out.println("Please, put in Name");
+            else
+                name = userName;
         }
-        void    setAge(int userAge){
-            age = userAge;
+        // Getter - возвращает значения
+        public String getName(){
+            return name;
         }
+
+        public void    setAge(int userAge){
+            if (userAge < 0)
+                System.out.println("Error! Incorrect age!");
+            else
+                age = userAge;
+        }
+        public int getAge(){
+            return age;
+        }
+
         void speak(){
             System.out.println("My name is "+name+", i'm "+age+" years old.");
         }
