05b3e82c0e41bbf01319d5469c210d6d8ed34ebf
prochell <prochell@student.21-school.ru>
2021-10-10 13:57:29 +0300

    add basic this

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,49 +1,33 @@
 public class MyProjects {
-    public static void main(String [] args) {
-        Person person1 = new Person();
-        person1.setName("Kira");
-        person1.setAge(15);
-        person1.sayHello();
-        person1.speak();
-        System.out.println(person1.getAge());
-        System.out.println(person1.getName());
+    public static void main(String[] args){
+        Human human1 = new Human();
+        human1.setAge(18);
+        human1.setName("Tom");
+        human1.getInfo();
     }
-    /*
-    * Пользователю необходимо давать только внешний интерфейс
-    * устройства программы, который не затрагивает значения переменных,
-    * к тому же можно ограничивать ввод
-    */
-    static class Person{
-        private String  name; //private - переменная видна только в пределах класса
-        private int     age;
+}
+// this - вызывает объект класса
+class Human{
+    String  name;
+    int     age;
 
-        // Setter - устанавливает значения
-        public void    setName(String userName){
-            if (userName.isEmpty())
-                System.out.println("Please, put in Name");
-            else
-                name = userName;
-        }
-        // Getter - возвращает значения
-        public String getName(){
-            return name;
-        }
-
-        public void    setAge(int userAge){
-            if (userAge < 0)
-                System.out.println("Error! Incorrect age!");
-            else
-                age = userAge;
-        }
-        public int getAge(){
-            return age;
-        }
-
-        void speak(){
-            System.out.println("My name is "+name+", i'm "+age+" years old.");
-        }
-        void    sayHello(){
-            System.out.println("Hello!");
-        }
+    public void setName(String name){
+        this.name = name;
+        /*
+        * Где this.name - относится к переменной класса, а
+        * name - к переменной метода
+        */
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void getInfo(){
+        System.out.println(name+", "+age);
     }
 }
