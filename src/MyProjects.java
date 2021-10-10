d7c256e35fa44d7336f6821564c81d3fd4a61900
Nike2406 <locus2@mail.ru>
2021-10-10 20:54:51 +0300

    fullfill constructors

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,33 +1,41 @@
 public class MyProjects {
     public static void main(String[] args){
-        Human human1 = new Human();
+        Human human1 = new Human("Bob", 20);
+        // Если удалить пустой констроктор, то мы больше
+        // не сможем создавать новые элементы класса через
+        // пустые скобки
         human1.setAge(18);
         human1.setName("Tom");
-        human1.getInfo();
     }
 }
-// this - вызывает объект класса
 class Human{
+/*
+* Конструктор создается для создания значеий
+* 1. В отличие от метода, у конструктора нет
+* возваращаемого значения.
+* 2. Имя конструктора должно совпадать с именем
+* класса и начинается с большой буквы.
+*/
     String  name;
     int     age;
 
+    // constructor
+    public Human(){
+        this.name = "Имя по умолчанию";
+        this.age = 0;
+        System.out.println("Привет из первого конструктора!");
+    }
+    public Human(String name, int age){ // constructor
+        System.out.println("Привет из второго конструктора!");
+        this.name = name;
+        this.age = age;
+    }
+    // Перегрузка методов - множество методов с одинаковым называнием,
+    // но с разными параметрами
     public void setName(String name){
         this.name = name;
-        /*
-        * Где this.name - относится к переменной класса, а
-        * name - к переменной метода
-        */
     }
     public void setAge(int age){
         this.age = age;
-    }
-    public String getName(){
-        return name;
-    }
-    public int getAge(){
-        return age;
-    }
-    public void getInfo(){
-        System.out.println(name+", "+age);
     }
 }
