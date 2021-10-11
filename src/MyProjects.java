f377dd9074c9bc344e4fecf90bdde9a5b2426339
prochell <prochell@student.21-school.ru>
2021-10-11 12:57:35 +0300

    add final

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,37 +1,17 @@
+import jdk.incubator.vector.VectorOperators;
+
 public class MyProjects {
     public static void main(String[] args){
-        Human h1 = new Human("Bob", 40);
-        Human h2 = new Human("Tom", 30);
-        Human.printHi(); // Метод принадлежит к классу (а не к объекту)
-        h1.printNumberOfPeople();
-        h2.printNumberOfPeople();
+        final int X = 10;
+        System.out.println(X);
+        // x != 5;
     }
 }
-/*
-* Static - принадлежит к классу
-* */
-class Human{
 
-    String  name;
-    int     age;
+// final - константа
+class Test{
+    public static final int CONSTANT = 0;
+    // static, потому что константа, как правило,
+    // только одна для класса
 
-    private static int  countPeople; // для всех объектов одна переменная
-
-    public Human(String name, int age){ // constructor
-        this.name = name;
-        this.age = age;
-        countPeople++;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setAge(int age){
-        this.age = age;
-    }
-    public static void printHi(){
-        System.out.println("Hi!");
-    }
-    public void printNumberOfPeople(){
-        System.out.println("Number of people is "+countPeople);
-    }
 }
