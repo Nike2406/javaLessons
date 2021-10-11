f17e3e89173fcabf4ae86d9763003489d7793aca
prochell <prochell@student.21-school.ru>
2021-10-11 12:08:02 +0300

    add static

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,41 +1,37 @@
 public class MyProjects {
     public static void main(String[] args){
-        Human human1 = new Human("Bob", 20);
-        // Если удалить пустой констроктор, то мы больше
-        // не сможем создавать новые элементы класса через
-        // пустые скобки
-        human1.setAge(18);
-        human1.setName("Tom");
+        Human h1 = new Human("Bob", 40);
+        Human h2 = new Human("Tom", 30);
+        Human.printHi(); // Метод принадлежит к классу (а не к объекту)
+        h1.printNumberOfPeople();
+        h2.printNumberOfPeople();
     }
 }
-class Human{
 /*
-* Конструктор создается для создания значеий
-* 1. В отличие от метода, у конструктора нет
-* возваращаемого значения.
-* 2. Имя конструктора должно совпадать с именем
-* класса и начинается с большой буквы.
-*/
+* Static - принадлежит к классу
+* */
+class Human{
+
     String  name;
     int     age;
 
-    // constructor
-    public Human(){
-        this.name = "Имя по умолчанию";
-        this.age = 0;
-        System.out.println("Привет из первого конструктора!");
-    }
+    private static int  countPeople; // для всех объектов одна переменная
+
     public Human(String name, int age){ // constructor
-        System.out.println("Привет из второго конструктора!");
         this.name = name;
         this.age = age;
+        countPeople++;
     }
-    // Перегрузка методов - множество методов с одинаковым называнием,
-    // но с разными параметрами
     public void setName(String name){
         this.name = name;
     }
     public void setAge(int age){
         this.age = age;
+    }
+    public static void printHi(){
+        System.out.println("Hi!");
+    }
+    public void printNumberOfPeople(){
+        System.out.println("Number of people is "+countPeople);
     }
 }
