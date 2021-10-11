7e7bd83b5add8037f94cf8d2674dfe2c591fc932
prochell <prochell@student.21-school.ru>
2021-10-11 17:29:29 +0300

    smth add

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,29 +1,24 @@
 /*
-* В Java есть два типа объектов:
-*  - mutable - изменяемый
-*  - immutable - неизменяемый
-*  String - immutable type
+* Любой класс в Java наследуется от класса Object
 * */
 public class MyProjects {
     public static void main(String[] args){
-        String x = "Hello";
-        x.toUpperCase(); // - возвращает новую строку
-        System.out.println(x);
-        x = x.toUpperCase();
-        System.out.println(x);
+        Human h1 = new Human("Bob", 40);
+        System.out.println(h1);
+        h1.toString(); // возвращает хешкод созданного объекта
+    }
+}
+class  Human {
+    private String  name;
+    private int     age;
 
-        String str1 = "Hi";
-        String str2 = " my";
-        String str3 = " friend.";
-        String stringAll = str1+str2+str3;
-        // Выполняется сначала сложение первых двух и создается нрвоый элемент,
-        // потом к получившемуся эл-ту прибавляется третий (долго, затратно)
-        System.out.println(stringAll);
+    public Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
-        StringBuilder sb = new StringBuilder("Hello sb");// - mutable
-        System.out.println(sb.toString());
-        sb.append(" yo!"); // изменяется метод StringBuilder (быстрее, не выделяется новой пмяти*)
-        System.out.println(sb.toString());
+    public String toString(){ // переопределение метода объекта
+        return name+", "+age;
     }
 }
 
