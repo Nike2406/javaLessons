4271084b5c86bd4cd2770f60daafdb3ff75e2ff1
prochell <prochell@student.21-school.ru>
2021-10-11 13:24:43 +0300

    add StringBuilder

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,17 +1,29 @@
-import jdk.incubator.vector.VectorOperators;
-
+/*
+* В Java есть два типа объектов:
+*  - mutable - изменяемый
+*  - immutable - неизменяемый
+*  String - immutable type
+* */
 public class MyProjects {
     public static void main(String[] args){
-        final int X = 10;
-        System.out.println(X);
-        // x != 5;
+        String x = "Hello";
+        x.toUpperCase(); // - возвращает новую строку
+        System.out.println(x);
+        x = x.toUpperCase();
+        System.out.println(x);
+
+        String str1 = "Hi";
+        String str2 = " my";
+        String str3 = " friend.";
+        String stringAll = str1+str2+str3;
+        // Выполняется сначала сложение первых двух и создается нрвоый элемент,
+        // потом к получившемуся эл-ту прибавляется третий (долго, затратно)
+        System.out.println(stringAll);
+
+        StringBuilder sb = new StringBuilder("Hello sb");// - mutable
+        System.out.println(sb.toString());
+        sb.append(" yo!"); // изменяется метод StringBuilder (быстрее, не выделяется новой пмяти*)
+        System.out.println(sb.toString());
     }
 }
 
-// final - константа
-class Test{
-    public static final int CONSTANT = 0;
-    // static, потому что константа, как правило,
-    // только одна для класса
-
-}
