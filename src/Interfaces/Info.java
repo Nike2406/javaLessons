1da557f4d08f93bd48f9141c43f8fb6dc153617e
prochell <prochell@student.21-school.ru>
2021-10-12 21:57:50 +0300

    add access mods

--- src/Interfaces/Info.java
+++
@@ -1,7 +0,0 @@
-package Interfaces;
-
-public interface Info {
-    public void showInfo();
-    // В интерфейсах не может быть реализации методов,
-    // а только их сигнатуры.
-}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/*
+* 4 модификатора доступа:
+* public, private, default, protected
+* */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
