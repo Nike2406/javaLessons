871d1a86c4caf16bf639a33264722142341448f4
prochell <prochell@student.21-school.ru>
2021-10-13 19:17:35 +0300

    smth add

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,17 @@
 public class Test {
     public static void main(String[] args) {
-        // При наследовании типа Animal для объекта класса Dog,
-        // мы можешм создавать форму типа animal из объекта класса Dog()
-        Animal animal = new Dog();
-        animal.eat(); // - имеем доступ
-        // animal.bark(); - не имеем доступа
-        // Позднее связывание - реализация у потомка приоритетнее
-        Dog dog = new Dog();
-        dog.eat(); // - имеем доступ
-        dog.bark(); // - имеем доступа
-        System.out.println();
+       Dog dog = new Dog();
+       // Upcasting - восходящее преобразование,
+        // т.е. объект класса Dog стал ок animal
+        Animal animal = dog;
 
-        Animal animal1 = new Animal();
-        Dog dog1 = new Dog();
-        Cat cat1 = new Cat();
-        // Использование теста для разных типов
-        test(animal1);
-        test(dog1);
-        test(cat1);
+        // Downcasting - нисходящее преобразование
+        Dog dog2 = (Dog)animal;
+        dog2.bark();
+
+        Animal a = new Animal();
+        Dog dog3 = (Dog)a;
+        dog3.bark();
     }
 
     public static void test(Animal animal) {
