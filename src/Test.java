394ec8ad9b0b5a4cec0f242ff86621053a2d7f93
prochell <prochell@student.21-school.ru>
2021-10-13 13:39:02 +0300

    ff access modif-s

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,27 @@
+import Packege1.Person;
+
 /*
 * 4 модификатора доступа:
 * public, private, default, protected
 * */
 public class Test {
-    public static void main(String[] args) {
+    // public int id;
+    // разрешает доступ к полю везде на протяжении прокта
+    // В одном java-файле не может быть двух public классов
 
+    // private int id;
+    // разрешает доступ к полю только в классе, где он был объявлен
+
+    // default() - когда не ставится модификатор доступа
+    // class Test {}
+    // default() - доступно в пределах пакета
+
+    // protected - доступны всем подклассам, даже если они вне
+    // пакета, где они были задиклорированы (при условии
+    // наследования класса)
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Bob";
     }
 }
