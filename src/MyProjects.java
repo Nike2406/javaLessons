c2e8ca068ebc33bcc89435f7a0b42804c1031c52
prochell <prochell@student.21-school.ru>
2021-10-14 20:16:25 +0300

    add generics

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
-* Так как уже был создан класс Animal,
-* мы сразу можем создавать объекты этого класса
+ArrayList - хранит в себе объекты
 * */
 public class MyProjects {
-    public static void main(String[] args){
-//        Animal animal = new Animal();
-//        animal.eat();
-//        animal.sleep();
-        Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
-        dog.showName();
+    public static void main(String[] args) {
+        ///// Java 5 ////
+        List animals = new ArrayList();
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("frog");
+
+//      String animal = animals.get(1); - необходим downcast
+        // для корректновго вывода, тк все объекты наследуются от
+        // клсасса Object
+        String animal = (String) animals.get(1); // downcast
+        System.out.println(animal);
+
+        //// Generics ////
+        List<String> animals2 = new ArrayList<String>();
+        // в <> указывается тип, класс, который будет помещаться в List
+        animals2.add("cat");
+        animals2.add("dog");
+        animals2.add("frog");
+        String animal2 = animals2.get(1);
+
+
+        /////// Java 7 ///////
+        List<String> animals3 = new ArrayList<>();
+        // В правой части можно не указывать тип параметра
     }
 }
 
