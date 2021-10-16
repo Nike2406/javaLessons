7ce16e332eda8d318a597dd3563b786f036755a1
prochell <prochell@student.21-school.ru>
2021-10-16 20:55:50 +0300

    add File_reading

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,35 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-/*
-ArrayList - хранит в себе объекты
-* */
 public class MyProjects {
-    public static void main(String[] args) {
-        ///// Java 5 ////
-        List animals = new ArrayList();
-        animals.add("cat");
-        animals.add("dog");
-        animals.add("frog");
+    public static void main(String[] args) throws FileNotFoundException {
+        // File.separator - автоматически определяет систему
+        // и выводит правильный знак / \
+        String separator = File.separator;
+       String path = separator+"Users"+separator+"prochell"+
+        separator+"projects"+separator+"javaLessons"+separator+"test";
+        // Если файл лежит в корне, то проще
+        File file2 = new File("test2");
+       // File - выводит файл в переменную с адресом
+       File file = new File(path);
 
-//      String animal = animals.get(1); - необходим downcast
-        // для корректновго вывода, тк все объекты наследуются от
-        // клсасса Object
-        String animal = (String) animals.get(1); // downcast
-        System.out.println(animal);
-
-        //// Generics ////
-        List<String> animals2 = new ArrayList<String>();
-        // в <> указывается тип, класс, который будет помещаться в List
-        animals2.add("cat");
-        animals2.add("dog");
-        animals2.add("frog");
-        String animal2 = animals2.get(1);
-
-
-        /////// Java 7 ///////
-        List<String> animals3 = new ArrayList<>();
-        // В правой части можно не указывать тип параметра
+       // Scanner тажке может считывать файл
+       Scanner scanner = new Scanner(file);
+       while (scanner.hasNextLine()){
+           System.out.println(scanner.nextLine());
+       }
+       scanner.close();
     }
 }
 
