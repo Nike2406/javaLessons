db6573a14bbc78589ef1312aa7da524d37cab189
Nike2406 <locus2@mail.ru>
2022-08-11 21:00:18 +0300

    added project

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,5 +1,73 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyProjects {
-    public static void main(String [] args) {
-        System.out.println("Hello motherfuckers!");
+    /*
+     * producer - добавляет в очередь элементы
+     * consumer - забирает из очереди элементы
+     * */
+    public static void main(String[] args) throws InterruptedException {
+        ProducerConsumer pc = new ProducerConsumer();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    pc.produce();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    pc.consume();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+}
+
+class ProducerConsumer {
+    private Queue<Integer> queue = new LinkedList<>();
+    private final int LIMIT = 10;
+    private final Object lock = new Object();
+
+    public void produce() throws InterruptedException {
+        int value = 0;
+        while (true) {
+            synchronized (lock) {
+                while (queue.size() == LIMIT) { // не if для безопасности
+                    lock.wait();
+                }
+                queue.offer(value++);
+                lock.notify();
+            }
+        }
+    }
+
+    public void consume() throws InterruptedException {
+        while (true) {
+            synchronized (lock) {
+                while (queue.size() == 0) { // не if для безопасности
+                    lock.wait();
+                }
+                int value = queue.poll();
+                System.out.println(value);
+                System.out.println("Queue size is " + queue.size());
+                lock.notify();
+            }
+            Thread.sleep(1000);
+        }
     }
 }
