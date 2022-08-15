1096bda0b44cb9b2ac3d30fb33973f7028375c2e
Nike2406 <locus2@mail.ru>
2022-08-15 21:46:24 +0300

    added project

--- src/MyProjects.java
+++ src/MyProjects.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class MyProjects {
     public static void main(String [] args) {
-        System.out.println("Hello motherfuckers!");
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        myThread.shutdown();
+    }
+}
+
+class MyThread extends Thread {
+     /*
+     volatile - настраивает синхронизацию потоков,
+        когда происходит чтение из одной переменной
+        Предотвращает когеренцию кешей, когда процессорное ядро работает
+        со старым кешом
+    */
+    private volatile boolean running = true;
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void shutdown() {
+        this.running = false;
     }
 }
